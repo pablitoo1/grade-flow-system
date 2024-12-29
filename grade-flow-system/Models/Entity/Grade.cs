d9f63883db6e8dd407edc8850124efb09194b104
Paweł Buczek <pbuczek7@wp.pl>
2024-12-29 14:01:27 +0100

    fix: #1 grade entity modified

--- grade-flow-system/Models/Entity/Grade.cs
+++ grade-flow-system/Models/Entity/Grade.cs
@@ -7,11 +7,11 @@ public class Grade
 {
     [Key] public int Id { get; set; }
     [Required] public int GradeTypeId { get; set; }
-    [ForeignKey("GradeTypeId")] public GradeType GradeType { get; set; }
+    [ForeignKey("GradeTypeId")] public GradeType? GradeType { get; set; }
     public DateTime DateAssigned { get; set; }
     [MaxLength(500)] public string? Comments { get; set; }
     [Required] public int StudentId { get; set; }
-    [ForeignKey("StudentId")] public Student Student { get; set; }
+    [ForeignKey("StudentId")] public Student? Student { get; set; }
     [Required] public int SubjectId { get; set; }
-    [ForeignKey("SubjectId")] public Subject Subject { get; set; }
+    [ForeignKey("SubjectId")] public Subject? Subject { get; set; }
 }
