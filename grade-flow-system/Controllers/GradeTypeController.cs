36b6117bd283f5355d58176f820bdbe72824c48d
Paweł Buczek <pbuczek7@wp.pl>
2025-01-07 16:23:31 +0100

    feat: #2 added crud for grade type

--- grade-flow-system/Controllers/GradeTypeController.cs
+++ grade-flow-system/Controllers/GradeTypeController.cs
@@ -1,4 +1,5 @@
-﻿using grade_flow_system.Models.DTO.GradeType;
+﻿using System.ComponentModel.DataAnnotations;
+using grade_flow_system.Models.DTO.GradeType;
 using grade_flow_system.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,11 +17,28 @@ public class GradeTypeController(GradeTypeService gradeTypeService) : Controller
     }
 
     /// <summary>Add new grade type</summary>
-    /// <response code ="400">Grade type already exists</response>
+    /// <response code ="400">Grade type value already exists</response>
     [HttpPost]
     public void add([FromBody] GradeTypeRequest gradeTypeRequest)    //FromQuery bierze z parametrów
     {
         gradeTypeService.add(gradeTypeRequest);
+    }
+
+    /// <summary>Edit existing grade type</summary>
+    /// <response code="400">Grade type value already exits</response>
+    /// <response code="404">Grade type not found</response>
+    [HttpPatch]
+    public void edit([FromBody] GradeTypeRequest gradeTypeRequest, [FromQuery][Required] int id)
+    {
+        gradeTypeService.edit(gradeTypeRequest, id);
+    }
+
+    /// <summary>Delete existing grade type</summary>
+    /// <response code="404">Grade type not found</response>
+    [HttpDelete]
+    public void delete([FromQuery][Required] int id)
+    {
+        gradeTypeService.delete(id);
     }
 }
 
